2623d9057125e3e3dbf3527108b1aa37e2196440
Qdesai <qaasimdesai786@gmail.com>
2022-11-03 02:54:33 -0400

    added 3 new projects to and moved the data folder to the QaasimBooks.DataAccess

---
+++ QaasimBooks.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QaasimBooks.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ QaasimBooks.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QaasimBooks.Utility
+{
+    public class Class1
+    {
+    }
+}
